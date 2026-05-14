26ef03940eb5269fde5626cc09a7eea267b261be
HERRMATTT <mattplay2903@gmail.com>
2026-05-14 15:19:22 -0300

    Implement basic arithmetic operations with user input
    Added user input for two numbers and calculated their sum and difference.

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,13 @@ def division (a,b):
     if b==0:
         return "Error, division por 0"
     return a/b
+
+
+num1= int(input("Ingrese el primer numero"))
+num2= int(input("Ingrese el segundo numero"))
+suma= num1 + num2
+resta= num1 - num2
+print ("La suma es:")
+print (suma)
+print("La resta es:")
+print (resta)
